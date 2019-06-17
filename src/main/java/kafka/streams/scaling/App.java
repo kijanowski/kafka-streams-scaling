d29691f51054658e3f6e146a82ea4cc9dfff566c
Jaroslaw Kijanowski <kijanowski@gmail.com>
2019-06-18 00:01:16 +0200

    First draft

---
+++ src/main/java/kafka/streams/scaling/App.java
@@ -0,0 +1,59 @@
+package kafka.streams.scaling;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.log4j.Logger;
+
+public class App {
+
+  static final String DONE = "done";
+  private static final Logger log = Logger.getLogger(App.class);
+
+  public static void main(String[] args) {
+
+    Properties config = new Properties();
+    config.put(StreamsConfig.APPLICATION_ID_CONFIG, "ks-scaling-app-app-id");
+    config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    config.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100);
+    config.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+    config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
+    config.put(ConsumerConfig.METRICS_SAMPLE_WINDOW_MS_CONFIG, 10000);
+
+    StreamsBuilder builder = new StreamsBuilder();
+
+    builder
+      .stream("inScalingTopic", Consumed.with(Serdes.String(), Serdes.String()))
+      .peek((key, value) -> {
+        try {
+          Thread.sleep(10);
+        } catch (InterruptedException e) {
+          log.error(e);
+        }
+      })
+      .filter((key, value) -> DONE.equals(value))
+      .peek((key, value) -> log.info("Done with key " + key))
+      .to("outScalingTopic", Produced.with(Serdes.String(), Serdes.String()))
+    ;
+
+    KafkaStreams streams = new KafkaStreams(builder.build(), config);
+    streams.start();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      try {
+        streams.close();
+        log.info("Stream stopped");
+      } catch (Exception exc) {
+        log.error("Got exception while executing shutdown hook: ", exc);
+      }
+    }));
+  }
+}
+
---
+++ src/main/java/kafka/streams/scaling/Sender.java
@@ -0,0 +1,51 @@
+package kafka.streams.scaling;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.log4j.Logger;
+
+public class Sender {
+
+  private static final Logger LOG = Logger.getLogger(Sender.class);
+
+  public static void main(String[] args) {
+    Properties properties = new Properties();
+    properties.put("bootstrap.servers", "localhost:9092");
+    properties.put("acks", "all");
+    properties.put("retries", 0);
+    properties.put("batch.size", 16384);
+    properties.put("linger.ms", 1);
+    properties.put("buffer.memory", 33554432);
+
+    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+    Producer<String, String> producer = new KafkaProducer<>(properties);
+
+    List<String> keys = Arrays.asList(UUID.randomUUID().toString(), UUID.randomUUID().toString(), UUID.randomUUID().toString());
+    LOG.info("Start");
+    for (String key : keys) {
+      for (int i = 0; i < 10000; i++) {
+        send(producer, key, "val" + i);
+      }
+      send(producer, key, App.DONE);
+    }
+
+    LOG.info("Closing Kafka Producer");
+    producer.close();
+  }
+
+  private static void send(Producer<String, String> producer, String key, String value) {
+    ProducerRecord<String, String> record = new ProducerRecord<>("inScalingTopic", key, value);
+    producer.send(record);
+  }
+
+}
